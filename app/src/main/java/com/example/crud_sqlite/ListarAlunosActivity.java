b5ff6fbe9d488948e0d4d5b90b8fb2d06c18524b
lucasdebeterco <lucasdebeterco@gmail.com>
2022-05-25 21:27:23 -0300

    Adicionado menu principal

--- app/src/main/java/com/example/crud_sqlite/ListarAlunosActivity.java
+++ app/src/main/java/com/example/crud_sqlite/ListarAlunosActivity.java
@@ -3,7 +3,11 @@ package com.example.crud_sqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -28,6 +32,24 @@ public class ListarAlunosActivity extends AppCompatActivity {
         alunosFiltrados.addAll(alunos);
         ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
         listView.setAdapter(adapter);
+    }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater i = getMenuInflater();
+        i.inflate(R.menu.menu_principal,menu);
+        return true;
+    }
+
+    public void cadastrar(MenuItem item) {
+        Intent it = new Intent(this, MainActivity.class);
+        startActivity(it);
+    }
+
+    public void onResume() {
+        super.onResume();
+        alunos = dao.obterTodos();
+        alunosFiltrados.clear();
+        alunosFiltrados.addAll(alunos);
+        listView.invalidateViews();
     }
 }
